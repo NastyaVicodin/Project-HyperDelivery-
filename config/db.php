36db1826673bf244f2e2a6639ba7b027dafbe9f5
Dmitriy Chechel <Betelgeuse1920@gmail.com>
2017-06-02 02:21:12 +0300

    Config update

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=delivery',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=g04bns9q8pfqs5o8',
+    'username' => 'n2qsgnfih1o8kxbf',
+    'password' => 'bqhipb4d5cr38uvo',
     'charset' => 'utf8',
 ];
--- config/web.php
+++ config/web.php
@@ -1,11 +1,10 @@
 <?php
-
 $params = require(__DIR__ . '/params.php');
-
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+	'defaultRoute' => 'orders/active_orders',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -27,8 +26,8 @@ $config = [
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com', // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'betelgeuse1920@gmail.com',
-                'password' => 'emsyov5r3arg',
+                'username' => 'room217test@gmail.com',
+                'password' => 'adminadmin',
                 'port' => '587',
                 'encryption' => 'tls', // Port 25 is a very common port too
                 //   'encryption' => 'tls', // It is often used, check your provider or mail server specs
@@ -37,7 +36,6 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'delivery/error',
         ],
-
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
@@ -48,30 +46,25 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-
             ],
         ],
         
     ],
     'params' => $params,
 ];
-
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
-
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
     ];
 }
-
 return $config;
